bf2f7b2deb18496d53e03d098e2b6f7013ca2ab7
Nathan Kluth <nlkluth@gmail.com>
2015-04-29 23:48:47 -0600

    when wall is initialized, show the context menu for it

--- Castles/Assets/Scripts/Wall/WallController.cs
+++ Castles/Assets/Scripts/Wall/WallController.cs
@@ -3,4 +3,16 @@ using System.Collections;
 
 public class WallController : MonoBehaviour 
 {
+	private Canvas canvas;
+
+	void Start()
+	{
+		ShowContextMenu();
+	}
+
+	private void ShowContextMenu()
+	{
+		canvas = gameObject.GetComponentInChildren<Canvas>();
+		canvas.enabled = true;
+	}
 }
