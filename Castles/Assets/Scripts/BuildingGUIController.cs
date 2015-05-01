09df3dd76bc3d947ec8df5d7ba3104c28686c4b7
Nathan Kluth <nlkluth@gmail.com>
2015-05-01 15:53:56 -0600

    working on hovering the rotation

---
+++ Castles/Assets/Scripts/BuildingGUIController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildingGUIController : MonoBehaviour 
+{
+	private Vector3 localTransform;
+
+	void Start()
+	{
+		localTransform = transform.position;
+	}
+
+	void Update()
+	{
+		hover();
+	}
+
+	private void hover()
+	{
+		float newPosition = Mathf.PingPong(15f, 20f);
+		transform.position = new Vector3(localTransform.x, newPosition, localTransform.y); 
+	}
+}
