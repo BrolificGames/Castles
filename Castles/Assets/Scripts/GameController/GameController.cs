375cd95db28a99e854c0e86f73ec4bb2cb55e41c
Nathan Kluth <nlkluth@gmail.com>
2015-05-01 16:39:08 -0600

    stubbing out future methods

---
+++ Castles/Assets/Scripts/GameController/GameController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// This script handles mouse clicks and then delegates actions based on game state
+// (eg placing a building) and what was clicked on as well as touch input
+public class GameController : MonoBehaviour 
+{
+	void Update()
+	{
+
+	}
+}
--- Castles/Assets/Scripts/Wall/WallController.cs
+++ Castles/Assets/Scripts/Wall/WallController.cs
@@ -7,7 +7,16 @@ public class WallController : MonoBehaviour
 
 	void Start()
 	{
-//		ShowContextMenu();
+		ShowContextMenu();
+	}
+
+	
+	public void selected()
+	{
+		// show object is selected
+
+		// open context menu for info, and building-specific options eg: wall show anchors for creating more wall
+		// other building, research, upgrades etc <=== this can probably be a gui menu that is separate from gameobject
 	}
 
 	private void ShowContextMenu()
