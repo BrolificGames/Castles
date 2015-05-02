0ab85cc6ba499469b4f74e323593d16bad7b20e9
Nathan Kluth <nlkluth@gmail.com>
2015-05-02 11:57:03 -0600

    setting up namespace

--- Castles/Assets/Scripts/GameController/ResourceManager.cs
+++ Castles/Assets/Scripts/GameController/ResourceManager.cs
@@ -5,6 +5,8 @@ namespace Castles
 {
 	public static class ResourceManager
 	{
-
+		public static float ScrollSpeed { get { return 25; } }
+		public static float RotateSpeed { get { return 100; } }
+		public static int ScrollWidth { get { return 15; } }
 	}
 }
