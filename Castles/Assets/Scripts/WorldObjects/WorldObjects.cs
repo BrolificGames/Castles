4d590b7d50e5c45bd7e49c5c75b65c1d5a657ebe
Nathan Kluth <nlkluth@gmail.com>
2015-05-02 12:12:20 -0600

    setting world objects, moved buildings to subfolder and declared parent attributes

---
+++ Castles/Assets/Scripts/WorldObjects/WorldObjects.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldObjects : MonoBehaviour 
+{
+	public int cost, sellValue, hitPoints, maxHitPoints;
+	protected Player player;
+	protected string[] actions = {};
+	protected bool currentlySelected = false;
+
+	protected virtual void Awake() 
+	{
+		
+	}
+	
+	protected virtual void Start() 
+	{
+		player = transform.root.GetComponentInChildren< Player >();
+	}
+	
+	protected virtual void Update() 
+	{
+		
+	}
+
+	public void SetSelection(bool selected) 
+	{
+		currentlySelected = selected;
+	}
+
+	public string[] GetActions() 
+	{
+		return actions;
+	}
+	
+	public virtual void PerformAction(string actionToPerform)
+	{
+
+	}
+}
