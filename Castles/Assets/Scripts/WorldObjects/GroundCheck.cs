eb37ef50f2189173b8cbb03e1fd2dd63a2280804
Nathan Kluth <nlkluth@gmail.com>
2015-05-03 17:01:57 -0600

    setting up ground check for building placement

---
+++ Castles/Assets/Scripts/WorldObjects/GroundCheck.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundCheck : MonoBehaviour 
+{
+	public bool isClear {get; set;}
+	public float groundCheckRadius;
+
+	void Update()
+	{
+		isClear = checkGround();
+	}
+
+	private bool checkGround()
+	{
+		Collider[] hitColliders = Physics.OverlapSphere(transform, groundCheckRadius);
+		foreach (Collider collider in hitColliders)
+		{
+			if (collider.tag != "Ground")
+			{
+				return false;
+			}
+		}
+
+		// if we make it through all colliders and all are "Ground" then we are clear
+		return true;
+	}
+}
