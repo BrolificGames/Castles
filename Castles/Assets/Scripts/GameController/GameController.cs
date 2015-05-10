605b7d6014d0d1526a5e6c5ba00d87c81dcd55f9
Nathan Kluth <nlkluth@gmail.com>
2015-05-09 22:10:56 -0600

    static ints

--- Castles/Assets/Scripts/GameController/GameController.cs
+++ Castles/Assets/Scripts/GameController/GameController.cs
@@ -3,10 +3,6 @@ using System.Collections;
 
 public class GameController : MonoBehaviour 
 {
-	public int Wood { get; set; }
-	public int Food { get; set; }
-	public int Stone { get; set; }
-
 	void Update()
 	{
 
--- Castles/Assets/Scripts/GameController/ResourceManager.cs
+++ Castles/Assets/Scripts/GameController/ResourceManager.cs
@@ -8,5 +8,8 @@ namespace Castles
 		public static float ScrollSpeed { get { return 25; } }
 		public static float RotateSpeed { get { return 100; } }
 		public static int ScrollWidth { get { return 15; } }
+		public static int Wood { get; set; }
+		public static int Stone { get; set; }
+		public static int Food { get; set; }
 	}
 }
