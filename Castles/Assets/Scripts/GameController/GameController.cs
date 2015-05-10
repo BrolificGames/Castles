2787fdb392fc7a94ac00ad7a68f7289aa138d059
Nathan Kluth <nlkluth@gmail.com>
2015-05-09 22:10:00 -0600

    stubbing gamecontroller

---
+++ Castles/Assets/Scripts/GameController/GameController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour 
+{
+	public int Wood { get; set; }
+	public int Food { get; set; }
+	public int Stone { get; set; }
+
+	void Update()
+	{
+
+	}
+}
