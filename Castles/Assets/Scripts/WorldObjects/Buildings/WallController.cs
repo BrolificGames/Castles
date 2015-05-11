f7d0804374b65f961ecd0eb4cf0cca3ce53ff9b2
Nathan Kluth <nlkluth@gmail.com>
2015-05-11 14:56:43 -0600

    world object will handle select

--- Castles/Assets/Scripts/WorldObjects/Buildings/WallController.cs
+++ Castles/Assets/Scripts/WorldObjects/Buildings/WallController.cs
@@ -10,14 +10,6 @@ public class WallController : WorldObjects
 		base.Update();
 	}
 
-	public void selected()
-	{
-		// show object is selected
-
-		// open context menu for info, and building-specific options eg: wall show anchors for creating more wall
-		// other building, research, upgrades etc <=== this can probably be a gui menu that is separate from gameobject
-	}
-
 	private void ShowContextMenu()
 	{
 	}
