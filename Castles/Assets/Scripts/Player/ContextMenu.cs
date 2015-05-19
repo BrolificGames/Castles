d6949547924e3206eadc3c2c6c98306219802b57
Nathan Kluth <nlkluth@gmail.com>
2015-05-19 16:13:25 -0600

    setting up contextmenu controller

---
+++ Castles/Assets/Scripts/Player/ContextMenu.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class ContextMenu : MonoBehaviour 
+{
+	public Button okButton;
+	public Button cancelButton;
+	public GameObject containerPanel;
+
+	private static ContextMenu contextMenu;
+
+	public static ContextMenu Instance()
+	{
+		if (!contextMenu) 
+		{
+			contextMenu = FindObjectOfType(typeof (ContextMenu)) as ContextMenu;
+			if (!contextMenu)
+			{
+				Debug.LogError ("There needs to be one active ContextMenu script on a GameObject in your scene.");
+			}
+		}
+		
+		return contextMenu;
+	}
+
+	public void Choice(UnityAction okEvent, UnityAction cancelEvent)
+	{
+		containerPanel.SetActive(true);
+
+		okButton.onClick.RemoveAllListeners();
+		okButton.onClick.AddListener(okEvent);
+		okButton.onClick.AddListener(CloseMenu);
+
+		cancelButton.onClick.RemoveAllListeners();
+		cancelButton.onClick.AddListener(cancelEvent);
+		cancelButton.onClick.AddListener(CloseMenu);
+	}
+
+	public void CloseMenu()
+	{
+		containerPanel.SetActive(false);
+	}
+}
