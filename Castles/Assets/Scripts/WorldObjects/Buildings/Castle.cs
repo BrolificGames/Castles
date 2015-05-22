d4e4bf86772c70921b610e2e354e02607b9c0f28
Nathan Kluth <nlkluth@gmail.com>
2015-05-22 09:41:49 -0600

    stubbing castle

---
+++ Castles/Assets/Scripts/WorldObjects/Buildings/Castle.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Castle : WorldObjects 
+{
+	public void showCanvas()
+	{
+		// gather information about current kingdom and display
+	}
+}
