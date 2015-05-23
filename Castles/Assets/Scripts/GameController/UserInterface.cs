bb752cb2d3cc9a8d5d6fa26e84702437a855e28d
Nathan Kluth <nlkluth@gmail.com>
2015-05-22 18:35:14 -0600

    setting up user interface

---
+++ Castles/Assets/Scripts/GameController/UserInterface.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+
+public class UserInterface : MonoBehaviour 
+{
+	public Button backButton;
+	private static UserInterface userInterface;
+	
+	public static UserInterface Instance()
+	{
+		if (!userInterface) 
+		{
+			userInterface = FindObjectOfType(typeof (UserInterface)) as UserInterface;
+			if (!userInterface)
+			{
+				Debug.LogError ("There needs to be one active UserInterface script on a GameObject in your scene.");
+			}
+		}
+		
+		return userInterface;
+	}
+	
+	public void Choice(UnityAction backEvent)
+	{
+		// slide the menu out
+		
+		backButton.onClick.RemoveAllListeners();
+		backButton.onClick.AddListener(backEvent);
+		backButton.onClick.AddListener(CloseMenu);
+	}
+	
+	public void CloseMenu()
+	{
+		// slide the menu back
+	}
+}
