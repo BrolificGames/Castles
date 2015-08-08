45011e83ef3d2275f73c368adc743b775943e93a
Nathan Kluth <nlkluth@gmail.com>
2015-08-08 15:53:59 -0600

    removing unused method

--- Castles/Assets/Scripts/Player/Player.cs
+++ Castles/Assets/Scripts/Player/Player.cs
@@ -5,14 +5,4 @@ using System.Collections;
 public class Player : MonoBehaviour 
 {
 	public WorldObjects selectedObject { get; set; }
-
-	public void SetSelection(WorldObjects worldObject)
-	{
-		if (selectedObject != null)
-		{
-			selectedObject.SetSelection(false);
-		}
-
-		selectedObject = worldObject;
-	}
 }
